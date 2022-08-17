2e30258d3639ab839394d64ccc40b4b23e3d66e7
LucasVillaboa <LucasVillaboa>
2022-08-16 23:08:13 -0300

    first commit

---
+++ src/Holamundo.java
@@ -0,0 +1,10 @@
+/**
+ * Created by admin on 16/8/2022.
+ */
+public class Holamundo {
+
+    public static void main(String []args){
+        String texto = "Hola Mundo";
+        System.out.println(texto);
+    }
+}
