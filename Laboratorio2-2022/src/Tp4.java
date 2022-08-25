23ceef0a1426b4e1a5227ce24e01353cf555e6e5
LucasVillaboa <LucasVillaboa>
2022-08-24 22:48:18 -0300

    tp 4,5,6

---
+++ Laboratorio2-2022/src/Tp4.java
@@ -0,0 +1,20 @@
+package TrabajosPracticos;
+
+/**
+ * Created by admin on 23/8/2022.
+ */
+public class Tp4 {
+    public static void main(String[]args){
+
+        String nombre="Lucas";
+        int edad=30;
+        double salario = 80500.30;
+        Boolean carnet = true;
+
+        System.out.println(nombre);
+        System.out.println(edad);
+        System.out.println(salario);
+        System.out.println(carnet);
+
+    }
+}
